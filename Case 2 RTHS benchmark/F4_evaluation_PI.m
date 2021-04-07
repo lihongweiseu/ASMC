1f1d880387815c1b5052182bd66b0b389dda2578
lihongweiseu <40768158+lihongweiseu@users.noreply.github.com>
2021-04-07 11:28:09 +0800

    Update F4_evaluation_PI.m

--- Case 2 RTHS benchmark/F4_evaluation_PI.m
+++ Case 2 RTHS benchmark/F4_evaluation_PI.m
@@ -42,21 +42,6 @@ eval_crit = [J1(ii,1) J2(ii,1) J3(ii,1) J4(ii,1) J5(ii,1) J6(ii,1) J7(ii,1) J8(i
 
 %% NOTE: This portion of the file should not be modified.
 
-time = clock;
-time_test = strcat(num2str(time(1)),'_',... % Returns year 
-    num2str(time(2)),'_',... % Returns month 
-    num2str(time(3)),'_',... % Returns day 
-    num2str(time(4)),'_',... % returns hour 
-    num2str(time(5)),'_',... % returns min 
-    num2str(fix(time(6)))) ; 
-
-if ii == 1
-    save(strcat('.\Results\Data_',time_test,'_Nom_'),'t', 'Ref_Resp','Num_resp','X_m','eval_crit');
-else
-    save(strcat('.\Results\Data_',time_test,'_Pert_',num2str(ii-1)),'t', 'Ref_Resp','Num_resp','X_m','eval_crit');
-end
-
-
 % if max(abs(Force_act.Data)) >= 8900
 %     clc, error('ERROR. The actuator capacity was exceeded.')
 % end
